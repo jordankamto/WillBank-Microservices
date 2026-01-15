edcad4b4f4f9f05278b7a4ee595cce7f03318a5e
Jordan Kamto <jordankamto32@gmail.com>
2026-01-15 13:20:09 +0100

    ajout du skelette de code pour le microservice composite

---
+++ CompositeService/src/main/java/com/stevecompany/CompositeService/CompositeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stevecompany.CompositeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeServiceApplication.class, args);
+	}
+
+}
